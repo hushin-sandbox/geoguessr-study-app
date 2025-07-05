9c0cc1577225fd6e6fb32d8aa89a6e54fd894fd4
hushin <ngtv.hsn@gmail.com>
2025-07-05 13:44:04 +0900

    feat: 環境変数のロードを追加し、動的なVite設定を実装

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/',
-  server: {
-    port: 3000,
-    host: true,
-  },
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.VITE_BASE_PATH || '/',
+    server: {
+      port: 3000,
+      host: true,
+    },
+  });
+};
